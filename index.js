406d87360ee6740f9733a2e0de61ef1c52a14daa
Samiksusamik <sam.kipnis@gmail.com>
2023-09-28 10:17:34 -0400

    add something that i will not name

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const cardButtonArray = document.querySelectorAll('.card-button')
-console.log(cardButtonArray)
+const modal = document.querySelector(".modal");
+const modalButtons = document.querySelectorAll('.card-button')
 
-document.addEventListener('DOMContentLoaded', () => {
-    for(let cardButton in cardButtonArray) {
-        console.log(cardButtonArray[cardButton])
-        cardButton.addEventListener('onclick', e => {
-            console.log(cardButton)
-        })
-    }
-})
+
+// Get the button that opens the modal
+for(let button of modalButtons) {
+    button.onclick = function() {
+        modal.style.display = "block";
+      }
+}
+// Get the <span> element that closes the modal
+const span = document.querySelector('.close');
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
